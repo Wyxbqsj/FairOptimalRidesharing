edda4f739d2f00f136f1ee1cd6cc90be92c50638
wyxbqsj <2490395716@qq.com>
2021-09-16 20:45:28 +0800

    implement BFRM

--- orderPackage/BasicFRM.py
+++ orderPackage/BasicFRM.py
@@ -1,6 +1,54 @@
-from typing import List
+
 from datadeal.orderAndDriver import Order
 from preference.costSaving import cost_saving
-from orderPackage.GreedyFRM import GFRM
+from setting import *
+import sys
+from preference.passenger import Passenger
 
+
+def execute(prefs, recursion_limit=10000):
+	# adjust the system's recursion call depth limit
+	# system default is 1000
+	sys.setrecursionlimit(recursion_limit)
+
+	# SETUP --> CREATE PERSON OBJECTS
+	Passenger.setup(prefs)
+
+	# PHASE 1
+	for passenger in Passenger.ppl.values():
+		if len(passenger.current_prefs) > 0:
+			passenger.propose_to(passenger.current_prefs[0])  # propose to your top choice
+
+
+	# PHASE 2
+	current_passenger = Passenger.find_passenger_with_second_column()
+	num_rotations = 1    # number of rotations so far...
+	while current_passenger:
+		current_pref = current_passenger.current_prefs[1]
+		current_pref.cross_off(current_pref.current_prefs[-1])
+		# has_empty_column_after_phase_one = Passenger.empty_column()
+		# if has_empty_column_after_phase_one:
+		# 	break
+		num_rotations = num_rotations + 1
+		current_passenger = Passenger.find_passenger_with_second_column()
+
+	ppl_without_match = Passenger.who_wasnt_matched() #没有被匹配上的人
+
+
+	return Passenger.prefsMatrix('current')
+
+if __name__ == '__main__':
+	from datadeal.problem import ProblemInstance
+	problemInstance = ProblemInstance(data_path, 1000)
+	currentTime = problemInstance.startTime+60
+	orders, drivers = problemInstance.batch(currentTime)
+	T = cost_saving(orders)
+	prefs = {}
+	for i in T.keys():
+		li = []
+		for j in T[i]:
+			prefs.setdefault(i,li).append(j.match_id)
+
+	# execute the match!!
+	A = execute(prefs)
 
---
+++ preference/passenger.py
@@ -0,0 +1,130 @@
+from typing import List
+from datadeal.orderAndDriver import Order
+from preference.costSaving import cost_saving
+from orderPackage.GreedyFRM import GFRM
+
+
+
+class Passenger:
+    def __init__(self, name):
+        self.name = name
+        self.initial_prefs = []  # a passenger's initial set of preferences
+        self.current_prefs = []  # a passenger's current set of preferences
+
+    # class variable to store all the passengers
+    ppl = {}
+
+    # somebody is going to propose!!
+    def propose_to(self, somebody):
+        somebody.receive_proposal_from(self)
+
+    # when somebody receives a proposal in phase 1
+    def receive_proposal_from(self, somebody):
+        current_prefs = self.current_prefs
+
+        # need to cross off those behind current proposal
+        prefs_to_chop = current_prefs[(current_prefs.index(somebody) + 1):]
+        for passenger in prefs_to_chop:
+            self.cross_off(passenger)
+
+    # crosses off a potential match
+    def cross_off(self, passenger):
+        # removes each from each other's preference array
+        if passenger in self.current_prefs:
+            self.current_prefs.remove(passenger)
+        if self in passenger.current_prefs:
+            passenger.current_prefs.remove(self)
+
+        # initiate a new proposal
+        if len(passenger.current_prefs) > 0:
+            passenger.propose_to(passenger.current_prefs[0])
+
+
+    # find a passenger who still has a second column
+    # return False if there is no passenger left
+    @staticmethod
+    def find_passenger_with_second_column():
+        res = False
+        for passenger in Passenger.ppl.values():
+            if len(passenger.current_prefs) > 1:
+                res = passenger
+                break
+        return res
+
+    # return just the names of the people in the preference arrays
+    def getPrefs(self, time):
+        res = []
+        if time == 'initial':
+            for pref in self.initial_prefs:
+                res.append(pref.name)
+        else:
+            for pref in self.current_prefs:
+                res.append(pref.name)
+        return res
+
+    # generates the hash to display all people's preferences
+    @staticmethod
+    def prefsMatrix(time):
+        res = {}
+        for passenger_name, passenger_object in Passenger.ppl.items():
+            res[passenger_name] = passenger_object.getPrefs(time)
+        return res
+
+    @staticmethod
+    def setup(prefs):
+        # create ppl hash which have names as keys
+        # and the corresponding passenger objects as values
+        for passenger in prefs:
+            Passenger.ppl[passenger] = Passenger(passenger)
+
+        # add the appropriate passenger objects to their preference array
+        for passenger_name, passenger_object in Passenger.ppl.items():  # the passenger
+            for pref_name in prefs[passenger_name]:  # their current_prefs
+                # populate the initial preference array
+                passenger_object.initial_prefs.append(Passenger.ppl[pref_name])
+                # populate the current preference array
+                passenger_object.current_prefs.append(Passenger.ppl[pref_name])
+
+    # determines whether everybody was matched
+    @staticmethod
+    def who_wasnt_matched():
+        ppl_without_match = []
+        for passenger_name, passenger_object in Passenger.ppl.items():
+            if len(passenger_object.current_prefs) != 1:
+                # they don't have a match!
+                ppl_without_match.append(passenger_object)
+        return ppl_without_match
+
+    def better_prefs(self):
+        initial_prefs = self.initial_prefs
+        # pprint.pprint(initial_prefs)
+        final_prefs = self.current_prefs
+        match = final_prefs[0]
+        better_prefs = initial_prefs[:initial_prefs.index(match)]
+
+        def passenger_name_string(obj):
+            return obj.name
+
+        # print(self.name + "," + str(list(map(passenger_name_string,better_prefs))))
+        return better_prefs
+
+    @staticmethod
+    def was_the_match_stable():
+        stable = True
+        for my_name, my_object in Passenger.ppl.items():
+            my_better_ppl = my_object.better_prefs()
+            for passenger in my_better_ppl:
+                their_better_ppl = passenger.better_prefs()
+                if my_object in their_better_ppl:
+                    stable = False
+                    break
+        return stable
+
+    @staticmethod
+    def empty_column():
+        empty = False
+        for passenger in Passenger.ppl.values():
+            if len(passenger.current_prefs) == 0:
+                empty = True
+                break
+        return empty
